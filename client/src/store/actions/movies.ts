6aaa018ec8ed6ae958c2a9075b1b89a597e2a6f0
Fredrik Busklein <fredrbus@stud.ntnu.no>
2020-10-12 12:08:18 +0200

    Added JSDoc-comments to actions

--- client/src/store/actions/movies.ts
+++ client/src/store/actions/movies.ts
@@ -2,6 +2,11 @@ import { GET_MOVIES_FROM_DB, GET_MOVIES_ERROR } from './actionTypes';
 import api from '../../utilities/api';
 import { Dispatch } from 'react';
 
+/**
+ * An action that populates Redux' state with movies retrieved from the REST API.
+ * Should only be called once in the runtime of the application.
+ * If no movies are found, return the error-response.
+ */
 export const getMovies = () => async (dispatch: Dispatch<Object>) => {
   try {
     const res = await api.get('/movies');
--- client/src/store/actions/results.ts
+++ client/src/store/actions/results.ts
@@ -2,6 +2,11 @@ import { Dispatch } from "react";
 import { MovieType } from "../../types";
 import { SEARCH_BY_TITLE } from "./actionTypes";
 
+/**
+ * An action that filters the movies in our result-list by a search-word.
+ * @param search The search word in the query
+ * @param movies The list of movies we're searching from.
+ */
 export const searchMovieTitle = (search: string, movies: Array<MovieType>) => (
   dispatch: Dispatch<Object>
 ) => {
