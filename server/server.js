9da659633105575757decf6da85abeed9d03619d
Fredrik Busklein <fredrbus@stud.ntnu.no>
2020-10-13 14:37:40 +0200

    movieList and functionality
    Added movieList to the User-model and endpoints in the API.

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 // Import and assigns config variables
-const CONFIG = require("./CONFIG");
+const CONFIG = require('./CONFIG');
 const PORT = CONFIG.PORT;
 const SERVER_ADDRESS = CONFIG.SERVER_ADDRESS;
 
 // Movie route
-const movies = require("./routes/movies");
-const users = require("./routes/users");
+const movies = require('./routes/movies');
+const users = require('./routes/users');
 
 /**
  * Initialize express server as 'app' with express' body-parser as middleware.
@@ -22,21 +22,22 @@ app.use(express.json(), cors());
  * Connects mongoose to the MongoDB database.
  */
 mongoose
-  .connect("mongodb://" + SERVER_ADDRESS + "/movies", {
+  .connect('mongodb://' + SERVER_ADDRESS + '/movies', {
     useNewUrlParser: true,
     useCreateIndex: true, //get rid of error
     useUnifiedTopology: true,
+    useFindAndModify: false,
   })
   .then(() => {
-    console.log("Connected to MongoDB");
+    console.log('Connected to MongoDB');
   })
-  .catch((e) => console.log("Error connecting to MongoDB: " + e));
+  .catch((e) => console.log('Error connecting to MongoDB: ' + e));
 
 /**
  * Use the api/movies route
  */
-app.use("/api/movies", movies);
-app.use("/api/users", users);
+app.use('/api/movies', movies);
+app.use('/api/users', users);
 
 /**
  * Listen for incoming requests at port 4000.
