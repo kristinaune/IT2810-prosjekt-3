5b83a3b3f98a7d25afb13f17ceec8867db320e00
Kristin Aune <kraun@stud.ntnu.no>
2020-10-14 17:55:49 +0200

    Update MyListUnAuth.tsx

--- client/src/components/pages/MyList/MyListUnAuth.tsx
+++ client/src/components/pages/MyList/MyListUnAuth.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const Account = () => {
+const MyListUnAuth = () => {
   return <div className='container'>Not Authenticated</div>;
 };
 /*
@@ -9,4 +9,4 @@ const mapStateToProps = (state: any) => {
   return { isAuthenticated: state.user.isAuthenticated };
 };*/
 
-export default connect(null, {})(Account);
+export default connect(null, {})(MyListUnAuth);
