c4bb53d4dc609dc4a871f2dbe54bb9e680dab89b
Fredrik Busklein <fredrbus@stud.ntnu.no>
2020-10-25 15:46:57 +0100

    Fixed render issue on MovieItem

--- client/src/components/MovieItem.tsx
+++ client/src/components/MovieItem.tsx
@@ -5,10 +5,18 @@ import { set_display_movie } from '../store/actions/displayMovie';
 import './MovieItem.css';
 import '../App.css';
 
-const MovieItem = (movie: MovieType) => {
+const MovieItem = ({
+  movie,
+  set_display_movie,
+}: {
+  movie: MovieType;
+  set_display_movie: Function;
+}) => {
   const { poster, year, title } = movie;
+  console.log(movie);
+
   return (
-    <div className='movieItem'>
+    <div className='movieItem' onClick={() => set_display_movie(movie)}>
       <div className='poster'>
         <img alt='poster' src={poster} />
       </div>
