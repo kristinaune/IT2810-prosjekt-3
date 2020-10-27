e424a8ab5206a6d6fde815458fcc18ec71d49889
Tiril <tirilsys@gmail.com>
2020-10-27 17:08:30 +0100

    Responsive modal and movielist

--- client/src/components/MovieModal.tsx
+++ client/src/components/MovieModal.tsx
@@ -29,7 +29,9 @@ const MovieModal = ({
     <React.Fragment>
       <div className='overlay' onClick={() => closeModal()} />
       <div className='modal'>
-        <button className = 'closeButton' onClick={() => closeModal()}>X</button>
+        <button className='closeButton' onClick={() => closeModal()}>
+          X
+        </button>
         <div className='content'>
           <h3>{title}</h3>
           <div className='modalPosters'>
@@ -37,28 +39,39 @@ const MovieModal = ({
           </div>
           <div className='modalInformation'>
             <h4>Plot: {plot}</h4>
-            <h4> Raiting: {rating}</h4>
+            <h4> Rating: {rating}</h4>
             <h4> Runtime: {runtime} min</h4>
             <h4> Director: {director}</h4>
             <h4> Genres: {genres}</h4>
             <h4> Actors: {actors}</h4>
-            <h4> Year: {year} </h4>   
-            {user && user.authState[0] ? (user.movieList?.includes(movie.imdbId) ?  <button className='movieListButton' onClick={(e) => {
-              e.preventDefault();
-              removeMovie(movie.imdbId, user.email);
-            }}
-> Remove from movielist</button> : <button className='movieListButton' onClick={(e) => {
-              e.preventDefault();
-              addMovie(movie.imdbId, user.email);
-            }}
-> Add to movielist</button>) : null}
-{}
-
-
-  
-    
+            <h4> Year: {year} </h4>
+            {user && user.authState[0] ? (
+              user.movieList?.includes(movie.imdbId) ? (
+                <button
+                  className='movieListButton'
+                  onClick={(e) => {
+                    e.preventDefault();
+                    removeMovie(movie.imdbId, user.email);
+                  }}
+                >
+                  {' '}
+                  Remove from movielist
+                </button>
+              ) : (
+                <button
+                  className='movieListButton'
+                  onClick={(e) => {
+                    e.preventDefault();
+                    addMovie(movie.imdbId, user.email);
+                  }}
+                >
+                  {' '}
+                  Add to movielist
+                </button>
+              )
+            ) : null}
+            {}
           </div>
-
         </div>
       </div>
     </React.Fragment>
