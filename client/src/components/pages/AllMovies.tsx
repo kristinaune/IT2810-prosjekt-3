8033619365c43056c126950900edb027ba56981f
Kristin Aune <kristinaune@Kristins-MacBook-Pro.local>
2020-10-27 09:23:04 +0100

    display all movies

--- client/src/components/pages/AllMovies.tsx
+++ client/src/components/pages/AllMovies.tsx
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { get_movies } from '../../store/actions/movies';
 import { connect } from 'react-redux';
 import { MovieType } from '../../types';
-import MovieCard from '../MovieItem';
+
 import paginator from '../../utilities/paginator';
 import './AllMovies.css';
-
+import MovieListItem from './Search/MovieListItem';
 /**
  * Displays all movies in database, by some filters
  * @param movies Array of movies to be displayed, from Redux
@@ -32,38 +32,21 @@ const AllMovies = ({
   return (
     <div className='movies'>
       <div className='filter'>
-        <button className='button1'> Comedy</button>
-        <button className='button1'> War</button>
-        <button className='button1'> Action</button>
-        <button className='button1'> SciFi</button>
-        <button className='button1'> Drama</button>
-        <button className='button1'> Crime</button>
+        <button className='genreButton'> Comedy</button>
+        <button className='genreButton'> War</button>
+        <button className='genreButton'> Action</button>
+        <button className='genreButton'> SciFi</button>
+        <button className='genreButton'> Drama</button>
+        <button className='genreButton'> Crime</button>
       </div>
-      {/* <div className='container'>
-        <header>
-          Filter your search
-          <button type='button' className='button'>
-            {' '}
-            Sjanger
-          </button>
-          <div className='dropdown'>
-            <ul>
-              <li>Comedy</li>
-              <li>Action</li>
-              <li>Scifi</li>
-              <li>Romantic</li>
-            </ul>
-          </div>
-        </header>
-      </div> */}
 
-      {movies &&
-        movies[0] &&
-        movies
-          .slice(0, Math.min(movieCount, movies.length))
+      <section>
+        {movies
+          ?.slice(0, Math.min(movieCount, movies.length))
           .map((movie: MovieType) => {
-            return <MovieCard key={movie.imdbId} movie={movie} />;
+            return movie && <MovieListItem key={movie.imdbId} movie={movie} />;
           })}
+      </section>
     </div>
   );
 };
