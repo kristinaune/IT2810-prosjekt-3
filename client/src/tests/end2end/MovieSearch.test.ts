a15094f90632c3679c1d8f10837e20ee3ddc6efd
Tiril <tirilsys@gmail.com>
2020-10-30 18:11:28 +0100

    Test for filter movie by "adventure"

--- client/src/tests/end2end/MovieSearch.test.ts
+++ client/src/tests/end2end/MovieSearch.test.ts
@@ -1,5 +1,5 @@
 import puppeteer from 'puppeteer';
-import api from '../../utilities/api';
+
 describe('Search for a movie and filter ', () => {
   test('Search for one specific movie', async () => {
     const browser = await puppeteer.launch();
@@ -38,6 +38,41 @@ describe('Search for a movie and filter ', () => {
     await page.waitForSelector('.closeButton');
     await page.click('.closeButton');
 
+    await browser.close();
+  });
+  test('Filter movies', async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto('http://localhost:3000');
+
+    await page.setViewport({ width: 1440, height: 788 });
+
+    await page.waitForSelector(
+      '.App > .navbar > .nav-menu > li:nth-child(2) > .nav-links'
+    );
+    await page.click(
+      '.App > .navbar > .nav-menu > li:nth-child(2) > .nav-links'
+    );
+
+    await page.waitForSelector(
+      '.App > .movies > .filterMovies > .filterButtons > .filterButton:nth-child(8)'
+    );
+    await page.click(
+      '.App > .movies > .filterMovies > .filterButtons > .filterButton:nth-child(8)'
+    );
+    await page.waitForSelector(
+      '.App > .movies > .movieList > .movieItem:nth-child(1)'
+    );
+    await page.click('.App > .movies > .movieList > .movieItem:nth-child(1)');
+
+    await page.waitForSelector('#root #title');
+    await page.click('#root #title');
+
+    console.log(await page.$eval('#title', (e) => e.innerHTML));
+    expect(await page.$eval('#title', (e) => e.innerHTML)).toBe(
+      'The Lord of the Rings: The Return of the King'
+    );
     await browser.close();
   });
 });
